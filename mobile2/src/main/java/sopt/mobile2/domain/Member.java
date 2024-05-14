d482a023614ccaa0be64ffa654f0b29c80bdda28
Hwan Jun Kim <spdlqjdkdlel1702@naver.com>
2024-05-14 22:55:43 +0900

    [feat] 스프링부트 및 도메인 설정

---
+++ mobile2/src/main/java/sopt/mobile2/domain/Member.java
@@ -0,0 +1,21 @@
+package sopt.mobile2.domain;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+public class Member {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @OneToMany
+    private List<Account> accountList = new ArrayList<>();
+}
---
+++ mobile2/src/main/java/sopt/mobile2/domain/Transfer.java
@@ -0,0 +1,24 @@
+package sopt.mobile2.domain;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+
+@Entity
+@Getter
+public class Transfer extends BaseTime{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    //송금 하는 계좌
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Account myAccount;
+
+    //송금 받는 계좌
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Account receiveAccount;
+
+    //거래액
+    private int transferAmount;
+}

fc2c37664d560cfa1bd2a4c83dfee58d91b9bf58
Hwan Jun Kim <spdlqjdkdlel1702@naver.com>
2024-05-23 11:55:01 +0900

    [fix] PaymentReesponseDto fix' monthlyTransferList'

--- mobile2/src/main/java/sopt/mobile2/dto/PaymentResponseDto.java
+++ mobile2/src/main/java/sopt/mobile2/dto/PaymentResponseDto.java
@@ -2,9 +2,9 @@ package sopt.mobile2.dto;
 
 import java.util.List;
 
-public record PaymentResponseDto(int payment, boolean isTotalOverZero, List<MonthlyTransferResponseDto> montlyTransferList) {
+public record PaymentResponseDto(int payment, boolean isTotalOverZero, List<MonthlyTransferResponseDto> monthlyTransferList) {
 
-    public static PaymentResponseDto of(int payment, boolean isTotalOverZero, List<MonthlyTransferResponseDto> montlyTransferList) {
-        return new PaymentResponseDto(payment, isTotalOverZero, montlyTransferList);
+    public static PaymentResponseDto of(int payment, boolean isTotalOverZero, List<MonthlyTransferResponseDto> monthlyTransferList) {
+        return new PaymentResponseDto(payment, isTotalOverZero, monthlyTransferList);
     }
 }
